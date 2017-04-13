e73ac02937dfcc40cd39dc063cbb74c2147f6d22
Gleb Abroskin <abroskingleb@gmail.com>
2017-04-13 22:59:47 +0300

    Binarized files
    Changes:
    - both test and train sets are now binarized

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from datetime import datetime
+from sklearn.preprocessing import LabelEncoder, label_binarize
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import LabelEncoder
 
 train_payments_file = 'data/qiwi_payments_data_train.csv'
 train_users_file = 'data/qiwi_users_data_train.csv'
@@ -72,17 +72,33 @@ def prepare_data():
     # encode universities with labels
     unis = set(train['university'])
     # unit 2 sets of names to get all of them
-    unis = unis.union(test['university'])
-    le.fit(pd.Series(list(unis)))
+    unis = pd.Series(list(unis.union(test['university'])))
+    le.fit(unis)
     # transformation
     train['university'] = le.transform(train[['university']])
     test['university'] = le.transform(test['university'])
+    unis_transformed = le.transform(unis)
+
+    # encode `university` with OneHotEncoding
+    train_uni_bin = label_binarize(train['university'], unis_transformed)
+    test_uni_bin = label_binarize(test['university'], unis_transformed)
+
+    train_uni_bin = pd.DataFrame(train_uni_bin)
+    test_uni_bin = pd.DataFrame(test_uni_bin)
+
+    # add binarized columns to DataFrames
+    train = pd.concat([train, train_uni_bin], axis=1)
+    test = pd.concat([test, test_uni_bin], axis=1)
+
+    # drop encoded columns
+    train.drop(['university'], axis=1)
+    test.drop(['university'], axis=1)
 
     # fill None in graduation year with 0s
     train['graduation_year'] = train['graduation_year'].fillna(value=0)
     test['graduation_year']  = test['graduation_year'].fillna(value=0)
 
-    # change `graduation_year` type to None
+    # change `graduation_year` type to int
     train['graduation_year'] = train['graduation_year'].astype(int)
     test['graduation_year'] = test['graduation_year'].astype(int)
 
