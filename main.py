00bbb1da87f0649de1855b3dc078af99776cfdab
Gleb Abroskin <abroskingleb@gmail.com>
2017-04-13 19:52:55 +0300

    Initial commit
    What's inside:
    - pptx with task
    - data/ -- folder with provided data
    - main.py can read files, parse dates and dump results to csv

---
+++ main.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from datetime import datetime
+
+train_payments_file = 'data/qiwi_payments_data_train.csv'
+train_users_file = 'data/qiwi_users_data_train.csv'
+# test files
+test_payments_file = 'data/qiwi_payments_data_test.csv'
+test_users_file = 'data/qiwi_users_data_test.csv'
+
+
+def load_train_set():
+    train_payments = pd.DataFrame.from_csv(train_payments_file, sep=';')
+
+    a = pd.read_csv(train_users_file, sep=';', parse_dates=[1])
+    train_users = pd.DataFrame(a)
+
+    train = pd.merge(train_users, train_payments, on='user_id')
+
+    train = parse_dates(train)
+
+    return train
+
+def load_test_set():
+    test_payments = pd.DataFrame.from_csv(test_payments_file, sep=';')
+
+    a = pd.read_csv(test_users_file, sep=';', parse_dates=[1])
+    test_users = pd.DataFrame(a)
+
+    test = pd.merge(test_users, test_payments, on='user_id')
+
+    test = parse_dates(test)
+
+    return test
+
+
+def parse_dates(dataset):
+    ''' Parses `date_month` to unix time.
+
+        One of the columns contain year and another month.
+    '''
+
+    dates = [datetime.strptime(dt, '%Y-%m') for dt in dataset['date_month']]
+
+    unix = [int(dt.timestamp()) for dt in dates]
+
+    dataset['unix_time'] = unix
+
+    dataset.drop(['date_month'], axis=1)
+
+    return dataset
+
+
+def dump_file(dataset):
+    pass    
+
+
+if __name__ == '__main__':
+    train = load_train_set()
+    test  = load_test_set()
+
+    # dump files if needed
+    train.to_csv('data/train.csv', index=False)
+    train.to_csv('data/test.csv', index=False)
+
